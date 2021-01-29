30728234254fd47c4f70195101a398f721297cf6
ruanxianggeng <709596687@qq.com>
2021-01-29 23:26:39 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon"
+             v-for="item in page"
+             :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src=item.imgUrl alt="banner">
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票1111111111111111'
+      }, {
+        id: '0002',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票2'
+      }, {
+        id: '0003',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票3'
+      }, {
+        id: '0004',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票4'
+      }, {
+        id: '0005',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票5'
+      }, {
+        id: '0006',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票6'
+      }, {
+        id: '0007',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票7'
+      }, {
+        id: '0008',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票8'
+      }, {
+        id: '0009',
+        imgUrl: 'https://imgs.qunarzz.com/vc/fd/55/94/6c7152c2a8b35a9c49bb26ea25.png_92.png',
+        desc: '景点门票9'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+.icons >>> .swiper-container
+  height: 0
+  padding-bottom: 50%
+
+.icon
+  position: relative
+  overflow: hidden
+  float: left
+  width: 25%
+  height: 0
+  padding-bottom: 25%
+  background: #f0f8ff
+
+  .icon-img
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: .44rem
+    box-sizing: border-box
+    padding: .1rem
+
+    .icon-img-content
+      display: block
+      margin: 0 auto
+      height: 100%
+
+  .icon-desc
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    height: .44rem
+    line-height: .44rem
+    text-align: center
+    color: $darkTextcolor
+    ellipsis()
+</style>
